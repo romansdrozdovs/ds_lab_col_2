504e3411a7b615dd99ac00913c8ba430f9d82e09
romansdrozdovs <144131720+romansdrozdovs@users.noreply.github.com>
2024-04-25 12:28:53 +0000

    25.04.2024

--- heap.py
+++ heap.py
@@ -57,28 +57,40 @@ class MaxHeap:
 
 
 def find_kth_smallest(nums, k):
+    if k > len(nums) or k < 1:
+        return 'error'
+    my_heap = MaxHeap()  
+    for num in nums:
+        my_heap.insert(num)
+        if len(my_heap.heap) > k: my_heap.remove()
+    return my_heap.heap[0]
     ###################################
     ###                             ###
     ###                             ###
     ###                             ###
     ###################################
 
+arr = input("array:")
+arr = arr.split(' ')
+array = []
+for numbers in arr: array.append(int(numbers))
+idx = int(input("k number:"))
+result = find_kth_smallest(array, idx)
+print(result)
 
+# # Test cases
+# nums = [[3,2,1,5,6,4], [6,5,4,3,2,1], [1,2,3,4,5,6], [3,2,3,1,2,4,5,5,6]]
+# ks = [2, 3, 4, 7]
+# expected_outputs = [2, 3, 4, 5]
 
-
-# Test cases
-nums = [[3,2,1,5,6,4], [6,5,4,3,2,1], [1,2,3,4,5,6], [3,2,3,1,2,4,5,5,6]]
-ks = [2, 3, 4, 7]
-expected_outputs = [2, 3, 4, 5]
-
-for i in range(len(nums)):
-    print(f'Test case {i+1}...')
-    print(f'Input: {nums[i]} with k = {ks[i]}')
-    result = find_kth_smallest(nums[i], ks[i])
-    print(f'Output: {result}')
-    print(f'Expected output: {expected_outputs[i]}')
-    print(f'Test passed: {result == expected_outputs[i]}')
-    print('---------------------------------------')
+# for i in range(len(nums)):
+#     print(f'Test case {i+1}...')
+#     print(f'Input: {nums[i]} with k = {ks[i]}')
+#     result = find_kth_smallest(nums[i], ks[i])
+#     print(f'Output: {result}')
+#     print(f'Expected output: {expected_outputs[i]}')
+#     print(f'Test passed: {result == expected_outputs[i]}')
+#     print('---------------------------------------')
 
 
 """
